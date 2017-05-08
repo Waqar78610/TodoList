189b3a5ad8ea43ecea50e6a69adf1f3a3f595302
wk6 <wk6@afsconnect1.njit.edu>
2017-05-08 02:03:41 -0400

    first login

---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+	<h1> This is a login page</h1>
+	<form  method = "list" action"index.php"
+	<strong>Username:</strong><input type="text" name="reg_uname"
+	value=""/><br><br>
+	<strong> Password:</strong><input type="password" name="reg_password"
+	value=""/><br><br>
+	<input type ="hidden" name="action" value="test_user"><br>
+	<input type="submit" value="login"/>
+	</form>
+	<form action= "register.php">
+	<input type="submit" value="Register"/>
+</body>
+</html>
